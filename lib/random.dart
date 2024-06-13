eb280907f55b5fe6143ff9195b2202666f05189a
ljxxyxxg <leejy970103@gmail.com>
2024-06-13 12:59:17 +0900

    오늘의추천레시피-수정jun

--- lib/random.dart
+++ lib/random.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider/carousel_slider.dart'; // Import carousel_slider package
 
 class RecipeRandomizer extends StatefulWidget {
   @override
@@ -12,12 +12,15 @@ class RecipeRandomizer extends StatefulWidget {
 class _RecipeRandomizerState extends State<RecipeRandomizer> {
   List<Map> _products = [];
   int _current = 0;
-  Map _selectedIndex = {}; // 타입을 Map으로 변경
-  CarouselController _carouselController = CarouselController();
+  Map _selectedIndex = {}; // 선택된 인덱스를 저장하는 맵
+  late CarouselController
+      _carouselController; // Declare _carouselController with late keyword
 
   @override
   void initState() {
     super.initState();
+    _carouselController =
+        CarouselController(); // Initialize _carouselController
     loadRecipeData();
   }
 
@@ -32,10 +35,10 @@ class _RecipeRandomizerState extends State<RecipeRandomizer> {
     Random random = Random();
     String randomCategory = allCategories[random.nextInt(allCategories.length)];
     List<dynamic> categoryRawList = decodedData[randomCategory];
-    List<Map> categoryList = categoryRawList.cast<Map>(); // 타입 캐스팅 추가
-    if (categoryList.length >= 3) {
+    List<Map> categoryList = categoryRawList.cast<Map>();
+    if (categoryList.length >= 6) {
       categoryList.shuffle();
-      _products = categoryList.sublist(0, 3);
+      _products = categoryList.sublist(0, 6);
     } else {
       _products = categoryList;
     }
@@ -45,11 +48,10 @@ class _RecipeRandomizerState extends State<RecipeRandomizer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: _selectedIndex.isNotEmpty // Map이 비어있는지 확인
+      floatingActionButton: _selectedIndex.isNotEmpty
           ? FloatingActionButton(
               onPressed: () {
-                // onPressed 함수 구현
-                // 예: 선택된 레시피로 다음 단계로 이동하는 코드 추가
+                // 선택된 레시피로 다음 단계로 이동하는 코드 추가
                 // Navigator.push(context, MaterialPageRoute(builder: (context) => NextPage()));
               },
               child: Icon(Icons.arrow_forward_ios),
@@ -62,106 +64,79 @@ class _RecipeRandomizerState extends State<RecipeRandomizer> {
           '오늘의 추천 레시피',
           style: TextStyle(
             color: Colors.black,
-            fontSize: 20, // 예시로 추가된 텍스트 스타일링
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: CarouselSlider(
-          carouselController: _carouselController,
-          options: CarouselOptions(
-            height: 450.0,
-            aspectRatio: 16 / 9,
-            viewportFraction: 0.70,
-            enlargeCenterPage: true,
-            pageSnapping: true,
-            onPageChanged: (index, reason) {
+      body: GridView.builder(
+        padding: EdgeInsets.all(16),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: 0.8, // Adjust childAspectRatio for desired size
+        ),
+        itemCount: _products.length,
+        itemBuilder: (BuildContext context, int index) {
+          Map recipe = _products[index];
+          return GestureDetector(
+            onTap: () {
               setState(() {
-                _current = index;
+                _selectedIndex = _selectedIndex == recipe ? {} : recipe;
               });
             },
-          ),
-          items: _products.map((recipe) {
-            return Builder(
-              builder: (BuildContext context) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if (_selectedIndex == recipe) {
-                        _selectedIndex = {};
-                      } else {
-                        _selectedIndex = recipe;
-                      }
-                    });
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 300),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      border: _selectedIndex == recipe
-                          ? Border.all(color: Colors.blue.shade500, width: 3)
-                          : null,
-                      boxShadow: _selectedIndex == recipe
-                          ? [
-                              BoxShadow(
-                                color: Colors.blue.shade100,
-                                blurRadius: 30,
-                                offset: Offset(0, 10),
-                              )
-                            ]
-                          : [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.2),
-                                blurRadius: 20,
-                                offset: Offset(0, 5),
-                              )
-                            ],
-                    ),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 320,
-                            margin: EdgeInsets.only(top: 10),
-                            clipBehavior: Clip.hardEdge,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Image.asset(
-                              recipe['image'],
-                              fit: BoxFit.cover,
-                            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ClipRRect(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(20)),
+                    child: Image.asset(recipe['image'],
+                        fit: BoxFit.cover,
+                        height: 400 // Adjust height of image as needed
+                        ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          recipe['title'],
+                          style: TextStyle(
+                            fontSize: 18, // Adjust fontSize as needed
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(height: 20),
-                          Text(
-                            recipe['title'],
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          recipe['description'],
+                          style: TextStyle(
+                            fontSize: 14, // Adjust fontSize as needed
+                            color: Colors.grey.shade600,
                           ),
-                          SizedBox(height: 20),
-                          Text(
-                            recipe['description'],
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.grey.shade600,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                );
-              },
-            );
-          }).toList(),
-        ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
