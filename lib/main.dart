de80a18f11be3f354eae4ce0a1d994c1f4fab52a
mi-ci <stormeight@naver.com>
2024-06-14 02:13:33 +0900

    aws

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _SendMoneyState extends State<HomePage> {
 
   Future<void> uploadImage(
       File imageFile, GlobalKey<CameraPageState> cameraPageKey) async {
-    String apiUrl = 'https://complete-stud-dashing.ngrok-free.app/predict';
+    String apiUrl = 'http://52.79.69.209:5000/predict';
     var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
     request.files
         .add(await http.MultipartFile.fromPath('frame', imageFile.path));
