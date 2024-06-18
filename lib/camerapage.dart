82530042d60d4a6e51cc5accc941f0691921dc07
ljxxyxxg <leejy970103@gmail.com>
2024-06-18 12:27:52 +0900

    수정

--- lib/camerapage.dart
+++ lib/camerapage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart'; // Spinkit 임포트 추가
-import 'productcard.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:animate_do/animate_do.dart';
 
 class CameraPage extends StatefulWidget {
@@ -15,13 +14,13 @@ class CameraPageState extends State<CameraPage> {
   List? _displayedList;
   late String food;
   Key _imageKey = UniqueKey();
-  bool _isLoading = true; // 로딩 상태 변수 추가
+  bool _isLoading = true;
 
   void updateImage(Image newImage) {
     setState(() {
       _displayedImage = newImage;
       _imageKey = UniqueKey();
-      _isLoading = false; // 이미지 업데이트 후 로딩 상태 변경
+      _isLoading = false;
     });
   }
 
@@ -43,103 +42,110 @@ class CameraPageState extends State<CameraPage> {
       appBar: AppBar(
         title: Text('Camera Page'),
       ),
-      body: _isLoading // 로딩 중인지 확인
-          ? Center(
-              child: SpinKitSpinningCircle(
-                itemBuilder: (context, index) {
-                  return Center(
-                    child: Text(
-                      "🐶",
-                      style: TextStyle(fontSize: 40),
-                    ),
-                  );
-                },
+      body: Stack(
+        children: [
+          // 배경 이미지 설정
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets3/vgt_image.jpg'), // 여기에 사용할 이미지 경로를 넣어주세요
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop), // 여기서 투명도 조절
               ),
-            )
-          //  Center(
-          //     child: SpinKitFadingCircle(
-          //       // Spinkit을 사용하여 로딩 인디케이터 표시
-          //       color: Colors.blue, // 인디케이터 색상 설정
-          //       size: 50.0, // 인디케이터 크기 설정
-          //     ),
-          //   )
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: FadeInDown(
-                    key: _imageKey,
-                    from: 30,
-                    delay: Duration(milliseconds: 500),
-                    duration: Duration(milliseconds: 1000),
-                    child: _displayedImage ?? Text('Loading..'),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                if (food != null)
-                  Center(
-                    child: FadeInUp(
-                      from: 30,
-                      delay: Duration(milliseconds: 500),
-                      duration: Duration(milliseconds: 1000),
-                      child: Text(
-                        '$food가 인식되었네요!',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ),
-                  ),
-                SizedBox(
-                  height: 10,
-                ),
-                if (food != null)
-                  Center(
-                    child: FadeInUp(
-                      from: 30,
-                      delay: Duration(milliseconds: 800),
-                      duration: Duration(milliseconds: 1000),
-                      child: Text(
-                        '$food로 만들 수 있는 요리를 알아보러갈까요?',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ),
-                  ),
-                SizedBox(
-                  height: 10,
-                ),
-                if (food != null)
-                  FadeInUp(
-                    delay: Duration(milliseconds: 1500),
-                    duration: Duration(milliseconds: 1000),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                      child: Material(
-                        elevation: 5,
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.black,
-                        child: MaterialButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ProductCard(food: food),
-                              ),
-                            );
-                          },
-                          minWidth: double.infinity,
-                          height: 50,
-                          child: Text(
-                            "추천 요리 보러가기",
-                            style: TextStyle(color: Colors.white, fontSize: 16),
-                          ),
+            ),
+          ),
+          Center(
+            child: _isLoading
+                ? SpinKitSpinningCircle(
+                    itemBuilder: (context, index) {
+                      return Center(
+                        child: Text(
+                          "🍽️",
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      );
+                    },
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                        child: FadeInDown(
+                          key: _imageKey,
+                          from: 30,
+                          delay: Duration(milliseconds: 500),
+                          duration: Duration(milliseconds: 1000),
+                          child: _displayedImage ?? Text('Loading..'),
                         ),
                       ),
-                    ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      if (food != null)
+                        Center(
+                          child: FadeInUp(
+                            from: 30,
+                            delay: Duration(milliseconds: 500),
+                            duration: Duration(milliseconds: 1000),
+                            child: Text(
+                              '$food가 인식되었네요!',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      if (food != null)
+                        Center(
+                          child: FadeInUp(
+                            from: 30,
+                            delay: Duration(milliseconds: 800),
+                            duration: Duration(milliseconds: 1000),
+                            child: Text(
+                              '$food로 만들 수 있는 요리를 알아보러갈까요?',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      if (food != null)
+                        FadeInUp(
+                          delay: Duration(milliseconds: 1500),
+                          duration: Duration(milliseconds: 1000),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                            child: Material(
+                              elevation: 5,
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.black,
+                              child: MaterialButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ProductCard(food: food),
+                                    ),
+                                  );
+                                },
+                                minWidth: double.infinity,
+                                height: 50,
+                                child: Text(
+                                  "추천 요리 보러가기",
+                                  style: TextStyle(color: Colors.white, fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
                   ),
-              ],
-            ),
+          ),
+        ],
+      ),
     );
   }
 }
